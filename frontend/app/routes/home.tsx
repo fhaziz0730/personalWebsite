5048291dba1ac03ec6ef985d076928fdedfecb2c
fhaziz0730 <fhaziz073@gmail.com>
2026-06-04 16:15:25 -0400

    Added gamepulse logo and added a header to website that links to github

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -6,14 +6,24 @@ export function loader() {
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="text-center p-4">
-      <h1 className="text-2xl">Hello, {loaderData.name}</h1>
-      <a
-        className="block mt-2 text-blue-500 underline hover:text-blue-600"
-        href="https://reactrouter.com/docs"
-      >
-        React Router Docs
-      </a>
+    <div>
+      <div className="text-center p-4">
+        <a
+          className="block mt-2 text-blue-500 underline hover:text-blue-600"
+          href="https://github.com/fhaziz0730"
+        >
+          Github
+        </a>
+      </div>
+      <div className="text-center p-4">
+        <h1 className="text-2xl">Hello, {loaderData.name}</h1>
+        <a
+          className="block mt-2 text-blue-500 underline hover:text-blue-600"
+          href="https://reactrouter.com/docs"
+        >
+          React Router Docs
+        </a>
+      </div>
     </div>
   );
 }
