ca71ebe1ad50c58f2dce90844c6af9c8e50b5356
fhaziz0730 <fhaziz073@gmail.com>
2026-06-08 12:12:31 -0400

    Added a header to the github profile with an animation when you hover over the icon.

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -7,12 +7,16 @@ export function loader() {
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <div>
-      <div className="text-center p-4">
+      <div className="flex justify-end text-center p-4 bg-yellow-600">
         <a
-          className="block mt-2 text-blue-500 underline hover:text-blue-600"
+          className="block mt-2 transition-transform duration-500 ease-in-out hover:scale-150"
           href="https://github.com/fhaziz0730"
         >
-          Github
+          <img
+            className="h-6 w-12 object-contain transition-transform duration-500 ease-in-out hover:scale-150"
+            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+            alt="Github Profile Link"
+          />
         </a>
       </div>
       <div className="text-center p-4">
